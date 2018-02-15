fe51d5e415abf9a0b4edd1abe1d6bba3c61d66bb
gmontantez <gmontantez7@gmail.com>
2018-02-15 12:04:45 -0500

    Pizza Exercise with tax

--- pizza_exercise3.rb
+++ pizza_exercise3.rb
@@ -3,9 +3,10 @@ meats = ["pepperoni","sausage","ham"]
 veggies = ["mushrooms","black olives","green peppers"]
 toppings = ["anchovies","chicken","spinach"]
 sauces = ["tomato","alfredo"]
-other_options = ["extra cheese","double pepperoni"]
+other_options = ["extra cheese","double pepperoni","feta cheese"]
 size = ["small","medium","large"]
 delivery = ["yes","no"]
+
 
 def cost(size)
 	if size == "small"
@@ -17,9 +18,20 @@ def cost(size)
    end
 end
 
+def special_options(other_options)
+   if other_options == "extra cheese"
+      1.00
+   elsif other_options == "double pepperoni"
+      2.00
+   elsif other_options == "feta cheese"
+      3.00
+   end
+end
+
+
 def delivery_choice(delivery)
    if delivery == "yes"
-      3.00
+      3
    else 
       0
    end
@@ -29,29 +41,35 @@ end
 puts "How many pizzas would you like?"
 pizzas = gets.chomp.to_i
 
+tax = 1.07
+
 delivery_option = delivery.sample
 delivery_cost = delivery_choice(delivery_option)
 
 pizza = 1
-total_cost = 0
+subtotal = 0
 while pizza <= pizzas do
    pizza_size = size.sample
    pizza_cost = cost(pizza_size)
+   other = other_options.sample
+   other_cost = special_options(other)
    puts "\nPizza #{pizza}"
-   puts "So, you would like a #{pizza_size} pizza at $#{pizza_cost}."
+   puts "So, you would like a #{pizza_size} pizza at $#{sprintf("%0.02f", pizza_cost)}."
 	puts "You would like a #{crust.sample} crust, with #{sauces.sample} sauce, your toppings will be #{veggies.sample}, #{meats.sample} and #{toppings.sample}."  
-	puts "You would also like #{other_options.sample}.\n"
-   total_cost += pizza_cost
+	puts "You would also like #{other_options.sample} at $#{sprintf("%.02f", other_cost)}.\n"
+   subtotal += pizza_cost + other_cost 
+   total_cost = subtotal*tax
 	pizza +=1
 end
 
-puts "\nThe total cost is $#{total_cost}."
+puts "\nThe total cost including tax is $#{total_cost}."
 puts "\nYour choice for delivery is #{delivery_option}"
-puts "\nThe delivery cost is $#{delivery_cost}."
+puts "\nThe delivery cost is $#{sprintf("%.02f", delivery_cost)}."
+
 if delivery_option == "yes"
    puts "\nWhat would you like to tip?"
    tip = gets.chomp.to_i
 else
    tip = 0
 end
-puts "\nThe tip cost is $#{tip}."
+puts "\nThe tip amount is $#{tip}."
